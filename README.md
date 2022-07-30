da78f47025bdf69c3d48778f929e9b2e7a673550
ak5k <42914711+ak5k@users.noreply.github.com>
2022-07-30 04:24:59 +0300

    dev

---
+++ README.md
@@ -0,0 +1,13 @@
+# reacurl
+## libcurl Easy Interface partial bindings for REAPER ReaScript
+```
+Curl_EasyInit
+Curl_EasySetopt 
+Curl_EasyPerform  
+Curl_EasyCleanup
+.
+.
+.
+etc     	     	    
+```
+see [curl](https://www.github.com/curl/curl) docs/examples and include/curl/curl.h
--- examples/reacurl-get.lua
+++ examples/reacurl-get.lua
@@ -1,13 +1,12 @@
 ---
 curl = reaper.Curl_EasyInit()
-reaper.Curl_EasySetopt(curl, "URL","https://www.landoleet.org/old/gfx_test.lua")
---downloads url as file "reacurl-test"
+reaper.Curl_EasySetopt(curl, "URL", "https://www.landoleet.org/old/gfx_test.lua")
 
-retval, buf = reaper.Curl_EasyPerform(curl,nil,"reacurl-test")
+-- downloads url as file "reacurl-test"
+retval, buf = reaper.Curl_EasyPerform(curl, nil, "reacurl-test")
 
---gets url into memory
+-- gets url into memory
 retval, buf = reaper.Curl_EasyPerform(curl)
 
-
-reaper.Curl_EasyCleanup(curl)
+reaper.Curl_EasyCleanup(curl) -- always cleanup
 reaper.ShowConsoleMsg(buf)
