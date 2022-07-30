a46de15d8668bd34069f92142154bb432b25e245
ak5k <42914711+ak5k@users.noreply.github.com>
2022-07-30 15:34:34 +0300

    dev

--- examples/reacurl-get.lua
+++ examples/reacurl-get.lua
@@ -3,9 +3,9 @@ curl = reaper.Curl_EasyInit()
 reaper.Curl_EasySetopt(curl, "URL", "https://www.landoleet.org/old/gfx_test.lua")
 
 -- downloads url as file "reacurl-test"
-retval, buf = reaper.Curl_EasyPerform(curl, nil, "reacurl-test")
+retval = reaper.Curl_EasyPerform(curl, "reacurl-test", 1)
 
--- gets url into memory
+-- gets url into memory buf
 retval, buf = reaper.Curl_EasyPerform(curl)
 
 reaper.Curl_EasyCleanup(curl) -- always cleanup
